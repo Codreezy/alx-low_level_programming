7c35f83e68a459de06fb5d49269fca67b81c6b38
Codreezy <adamsstansfield@gmail.com>
2022-11-16 23:55:10 -0800

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,19 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int point;
-
-	for (; *haystack; haystack++)
+for (; *haystack; haystack++)
+{
+	for (point = 0; needle[point]; point++)
 	{
-		for (point = 0, needle[point]; point++)
+		if (!(*(haystack + point)))
 		{
-			if (!(*(haystack + point)))
-			{
-				return (NULL);
-			}
+			return (NULL);
+		}
 		if (*(haystack + point) != needle[point])
 			break;
-		}
-		if (needle[point] == '\0')
-			return (haystack);
 	}
-	return (NULL);
+	if (needle[point] == '\0')
+		return (haystack);
+}
+return (NULL);
 }
